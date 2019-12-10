e956b897aefdbee8839d93aff6c78f330117aa50
chachaeyun <58388423+chachaeyun@users.noreply.github.com>
2019-12-10 17:09:34 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ console.log('jquery...');
 
 $(function(){
   // 마우스 오버시 메뉴 열림
-  $('.depth1 > li').mouseover(function(){
+  $('.depth1').mouseover(function(){
     // class추가되면 보임
     $('.depth2').addClass('show');
+    $('.ex').addClass('show');
   });
 
   // 마우스가 나가면 메뉴 닫힘
   $('.depth1 > li').mouseout(function(){
     $('.depth2').removeClass('show');
+    $('.ex').removeClass('show');
   });
 
 });
