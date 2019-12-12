bd898a7f2838f7146becf6db12763d387de7b2ab
daa02 <34049789+daa02@users.noreply.github.com>
2019-12-12 16:00:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // main.js
 console.log('jquery...');
 
+// gnb
 $(function(){
   // 마우스 오버시 메뉴 열림
   $('.depth1').mouseover(function(){
@@ -15,4 +16,30 @@ $(function(){
     $('.ex').removeClass('show');
   });
 
+});
+
+// popup
+$(function(){
+  $('.p1').addClass('show');
+
+  // 닫기버튼클릭시 팝업창 종료
+  $('#btn_close1').click(function(){
+      $('.p1').removeClass('show');
+  });
+
+
+  $('.p2').addClass('show');
+
+  // 닫기버튼클릭시 팝업창 종료
+  $('#btn_close2').click(function(){
+      $('.p2').removeClass('show');
+  });
+
+  
+  $('.p3').addClass('show');
+
+  // 닫기버튼클릭시 팝업창 종료
+  $('#btn_close3').click(function(){
+      $('.p3').removeClass('show');
+  });
 });
--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,3 @@
-// $(function(){
-//     var count = 1; // 이미지 번호
-
-//     // 인터벌 함수(함수, 재생시간ms)
-//     setInterval(function(){
-              
-//       console.log(count);
-//       if(count > 2) count = 0;
-//       // 모든 이미지 비표시
-//       $('.slider img').removeClass('show');
-//       // 현재 이미지만 표시
-//       $('.slider img')
-//         .eq(count)
-//         .addClass('show');
-
-// //      $('.indicator > a').removeClass('show');
-// //      $('.indicator > a')
-// //        .eq(count)
-// //        .addClass('show');
-
-//       count++;
-//     }, 2000);
-
-//   });
-
-  
-
 $(function(){
   var count = 1; 
 
@@ -62,7 +35,7 @@ $(function(){
           .addClass('show');
   
           count++;
-      }, 2000);
+      }, 4000);
   };
 
   set_slider(0);
