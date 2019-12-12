d8bf69f53407385355f6a5e3e71ae4597c7d0414
daa02 <34049789+daa02@users.noreply.github.com>
2019-12-12 12:12:44 +0900

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,93 @@
-$(function(){
-    var count = 1; // 이미지 번호
+// $(function(){
+//     var count = 1; // 이미지 번호
 
-    // 인터벌 함수(함수, 재생시간ms)
-    setInterval(function(){
+//     // 인터벌 함수(함수, 재생시간ms)
+//     setInterval(function(){
               
-      console.log(count);
-      if(count > 2) count = 0;
-      // 모든 이미지 비표시
-      $('.slider img').removeClass('show');
-      // 현재 이미지만 표시
+//       console.log(count);
+//       if(count > 2) count = 0;
+//       // 모든 이미지 비표시
+//       $('.slider img').removeClass('show');
+//       // 현재 이미지만 표시
+//       $('.slider img')
+//         .eq(count)
+//         .addClass('show');
+
+// //      $('.indicator > a').removeClass('show');
+// //      $('.indicator > a')
+// //        .eq(count)
+// //        .addClass('show');
+
+//       count++;
+//     }, 2000);
+
+//   });
+
+  
+
+$(function(){
+  var count = 1; 
+
+  function set_slider(c){
+      $('.slider img, .indicator  a')
+          .removeClass('show');
+
+      count = c;
+
       $('.slider img')
-        .eq(count)
-        .addClass('show');
+          .eq(c)
+          .addClass('show');
 
-//      $('.indicator > a').removeClass('show');
-//      $('.indicator > a')
-//        .eq(count)
-//        .addClass('show');
+      $('.indicator  a')
+          .eq(c)
+          .addClass('show');
+  };
 
-      count++;
-    }, 3000);
 
+  function slider(){
+      setInterval(function(){
+          console.log(count);
+          if(count > 2) count = 0;
+          // 모든 이미지 비표시
+          $('.slider img').removeClass('show');
+          // 현재 이미지만 표시
+          $('.slider img')
+              .eq(count)
+              .addClass('show');
+          
+          // 인디게이터 이동
+          $('.indicator > a').removeClass('show');
+          $('.indicator > a')
+          .eq(count)
+          .addClass('show');
+  
+          count++;
+      }, 2000);
+  };
+
+  set_slider(0);
+  slider();
+
+  $('#in01').click(function(){
+      console.log("111")
+      set_slider(0);
+      return false;
   });
+
+  $('#in02').click(function(){
+      console.log("222")
+      set_slider(1);
+      return false;
+  });
+
+  $('#in03').click(function(){
+      console.log("333")
+      set_slider(2);
+      return false;
+  });
+  
+  
+
+  
+}); 
+
